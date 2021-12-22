b40077685c74a6f990c0df03f94628b4d2ec4781
earl-stephens <stephensearl@hotmail.com>
2021-12-21 20:55:20 -0700

    Set up a simple array of strings

--- src/application/App.java
+++ src/application/App.java
@@ -4,7 +4,15 @@ public class App {
 
 	public static void main(String[] args) {
 		
+		//An array can only hold one data type
+		
+		String[] foods = {"Potatoes", "Rice", "Pasta", "Bread"};
 
+		System.out.println(foods[0]);
+		System.out.println(foods[3]);
+		
+		foods[0] = "Onions";
+		System.out.println(foods[0]);
 	}
 
 }
